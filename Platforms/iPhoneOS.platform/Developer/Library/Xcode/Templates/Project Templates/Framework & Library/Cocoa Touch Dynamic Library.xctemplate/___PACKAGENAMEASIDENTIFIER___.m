516e8fd3956741c8bcfdeb775874519065476502
Du Song <freewizard@gmail.com>
2011-11-07 16:50:44 +0800

    first commit

---
+++ Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/Framework & Library/Cocoa Touch Dynamic Library.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -0,0 +1,23 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,12 @@
+build-iOS-dylib
+
+
+INSTALL
+=======
+Extract files to /Developer (or other directory having Xcode)
+
+
+COMPATIBILITY
+=============
+Tested with Xcode 4.2
+
